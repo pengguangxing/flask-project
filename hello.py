ec89dbf8efae54ce5d4497047a55182f0453ba02
pengguangxing <pengguangxing@163.com>
2017-09-03 21:44:40 +0800

    templates

--- hello.py
+++ hello.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 # flask基础学习
 
-from flask import Flask
+from flask import Flask, make_response,render_template
+from flask_script import Manager
 
 
 app = Flask(__name__)
+
+manager = Manager(app)
 
 app.config['debug'] = True
 
 
 @app.route('/')
-def index():
-    return '<h1>Hello,Test</h1>'
+@app.route('/<username>')
+def index(username=None):
+    if username is None:
+        name = 'World'
+    else:
+        name = username
+    resp = make_response(render_template('index.html', name=name))
+    resp.set_cookie('pengguangxing', '123456')
+    return resp
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
